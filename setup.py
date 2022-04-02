b1503422278dbfacbae37c9629d56c2c2b4d289b
athrvvv <codingbruh.ac@gmail.com>
2022-04-03 01:05:22 +0530

    Delete unwanted files

--- setup.py
+++
@@ -1,6 +0,0 @@
-from cx_Freeze import setup, Executable
-  
-setup(name = "BABU" ,
-      version = "1.0.0" ,
-      description = "A.I Functionality :)" ,
-      executables = [Executable("babu.py")])
