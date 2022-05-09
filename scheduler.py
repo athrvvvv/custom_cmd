e0009f6cda61bb87bff73b718665c3afb3a5be8d
athrvvv <codingbruh.ac@gmail.com>
2022-05-10 00:33:37 +0530

    Fixed bugs and given some Upgrades to all three functions

--- scheduler.py
+++ scheduler.py
@@ -3,14 +3,47 @@ import WConio2 as input_source
 from datetime import date
 now = date.today()
 str_today = str(now)
- 
+main_path = os.path.dirname(__file__)
+def priority_reader():
+    with open((os.path.join((os.path.dirname(main_path)),"hidden_works","priority_list.txt")),"r") as f:
+        print("PRIORITY AS BELOW:- ")
+        Lines = f.readlines()   
+        count = 0
+        for line in Lines:
+            count += 1
+            not_space = line.rstrip()
+            if not_space:
+                print("{}. {}".format(count, line.strip()))
+                
+    print()
+
+def set_on_off_prio(self):
+    with open((os.path.join(main_path,"autogenarated_files","prio_status.txt")), "w") as f:
+        status = self.lower()
+        f.write(status)
+        
+def check_prio_status():
+    with open((os.path.join(main_path,"autogenarated_files","prio_status.txt")), "r") as f:
+        read = f.read()
+        if read == ("on"):
+            priority_reader()
+        else:
+            StopIteration()
 def priority_list():
     check_file = os.path.isfile(r"C:\Users\athar\OneDrive\Documents\projects\hidden_works\priority_list.txt")
     if check_file == (False):
         os.system("echo> "+r"C:\Users\athar\OneDrive\Documents\projects\hidden_works\priority_list.txt")
-    with open(r"C:\Users\athar\OneDrive\Documents\projects\hidden_works\priority_list.txt", "r") as np_file:    
-            data = np_file.read()
-            number_of_characters = len(data)
+    with open((os.path.join((os.path.dirname(main_path)),"hidden_works","priority_list.txt")),"r") as f:
+        print("PRIORITY AS BELOW:- ")
+        data = f.read()
+        number_of_characters = len(data)
+        Lines = f.readlines()   
+        count = 0
+        for line in Lines:
+            count += 1
+            not_space = line.rstrip()
+            if not_space:
+                print("{}. {}".format(count, line.strip()))
     if number_of_characters == 12 or number_of_characters == 0:
         print()
         print("EMPTY FILE AF")
@@ -39,13 +72,16 @@ def priority_list():
             print("CLEAR THE ENTIRE CONTENT OF FILE..?")
             print("YES (Y) or NO (N)")
             confirmation = input_source.getkey()
-            if confirmation == ("n") or confirmation == ("n"):
+            if confirmation == ("n") or confirmation == ("N"):
                 priority_list()
-   
-            else:
+                break
+            elif confirmation == ("y") or confirmation == ("Y"):
                 file = open(r"C:\Users\athar\OneDrive\Documents\projects\hidden_works\priority_list.txt","w")
                 file.close()
                 print("CLEARED THE CONTENT OF "+ '"'+"priority_list.txt"+'"')
+            else:
+                priority_list()
+                break
         else:
             with open(r"C:\Users\athar\OneDrive\Documents\projects\hidden_works\priority_list.txt", "a") as np_file:
                 np_file.write(input0+ "\n")
@@ -84,13 +120,16 @@ def quick_note():
             print("CLEAR THE ENTIRE CONTENT OF FILE..?")
             print("YES (Y) or NO (N)")
             confirmation = input_source.getkey()
-            if confirmation == ("n") or confirmation == (""):
+            if confirmation == ("n") or confirmation == ("N"):
                 quick_note()
-   
-            else:
+                break
+            elif confirmation == ("y") or confirmation == ("Y"):
                 file = open(r"C:\Users\athar\OneDrive\Documents\projects\hidden_works\quick_note.txt","w")
                 file.close()
                 print("CLEARED THE CONTENT OF "+ '"'+"quick_note.txt"+'"')
+            else:
+                quick_note()
+                break
         else:
             with open(r"C:\Users\athar\OneDrive\Documents\projects\hidden_works\quick_note.txt", "a") as np_file:
                 np_file.write(input0+ "\n")
@@ -129,13 +168,16 @@ def roadmap():
             print("CLEAR THE ENTIRE CONTENT OF FILE..?")
             print("YES (Y) or NO (N)")
             confirmation = input_source.getkey()
-            if confirmation == ("n") or confirmation == (""):
-                roadmap()
-            
-            else:
+            if confirmation == ("n") or confirmation == ("N"):
+                roadmap()          
+                break
+            elif confirmation == ("y") or confirmation == ("Y"):
                 file = open(r"C:\Users\athar\OneDrive\Documents\projects\hidden_works\roadmap.txt","w")
                 file.close()
                 print("CLEARED THE CONTENT OF "+ '"'+"roadmap.txt"+'"')
+            else:
+                roadmap()
+                break
         else:
             with open(r"C:\Users\athar\OneDrive\Documents\projects\hidden_works\roadmap.txt", "a") as np_file:
                 np_file.write(input0+ "\n")
