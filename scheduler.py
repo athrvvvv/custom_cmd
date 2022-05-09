295732c39cbca2d48e2cae9ff5e9502f07f8d1f4
athrvvv <codingbruh.ac@gmail.com>
2022-05-10 01:52:59 +0530

    Fixed paths of files :)

--- scheduler.py
+++ scheduler.py
@@ -3,9 +3,11 @@ import WConio2 as input_source
 from datetime import date
 now = date.today()
 str_today = str(now)
+
 main_path = os.path.dirname(__file__)
+
 def priority_reader():
-    with open((os.path.join((os.path.dirname(main_path)),"hidden_works","priority_list.txt")),"r") as f:
+    with open((os.path.join((os.path.dirname(main_path)),"autogenarated_files","priority_list.txt")),"r") as f:
         print("PRIORITY AS BELOW:- ")
         Lines = f.readlines()   
         count = 0
@@ -13,8 +15,7 @@ def priority_reader():
             count += 1
             not_space = line.rstrip()
             if not_space:
-                print("{}. {}".format(count, line.strip()))
-                
+                print("{}. {}".format(count, line.strip())) 
     print()
 
 def set_on_off_prio(self):
@@ -30,10 +31,10 @@ def check_prio_status():
         else:
             StopIteration()
 def priority_list():
-    check_file = os.path.isfile(r"C:\Users\athar\OneDrive\Documents\projects\hidden_works\priority_list.txt")
+    check_file = os.path.isfile(os.path.join(main_path,"autogenarated_files","priority_list.txt"))
     if check_file == (False):
-        os.system("echo> "+r"C:\Users\athar\OneDrive\Documents\projects\hidden_works\priority_list.txt")
-    with open((os.path.join((os.path.dirname(main_path)),"hidden_works","priority_list.txt")),"r") as f:
+        open((os.path.join(main_path,"autogenarated_files","priority_list.txt")),"w").close()
+    with open((os.path.join(main_path,"autogenarated_files","priority_list.txt")),"r") as f:
         print("PRIORITY AS BELOW:- ")
         data = f.read()
         number_of_characters = len(data)
@@ -44,7 +45,8 @@ def priority_list():
             not_space = line.rstrip()
             if not_space:
                 print("{}. {}".format(count, line.strip()))
-    if number_of_characters == 12 or number_of_characters == 0:
+    
+    if number_of_characters == 0:
         print()
         print("EMPTY FILE AF")
         print()
@@ -57,15 +59,15 @@ def priority_list():
         if input0 == (""):
             break
         elif input0 == ("open"):
-            os.startfile(r"C:\Users\athar\OneDrive\Documents\projects\hidden_works\priority_list.txt")
+            os.startfile(os.path.join(main_path,"autogenarated_files","priority_list.txt"))
             break
         elif "*" in input0:
             replace_task = input0.replace("*"," ")
-            with open(r"C:\Users\athar\OneDrive\Documents\projects\hidden_works\priority_list.txt", "a") as np_file:
+            with open(((os.path.join(main_path,"autogenarated_files","priority_list.txt"))),"a") as np_file:
                 np_file.write(replace_task+ "\n")
                 break
         elif input0 == ("read"):
-            with open(r"C:\Users\athar\OneDrive\Documents\projects\hidden_works\priority_list.txt", "r") as np_file:
+            with open(((os.path.join(main_path,"autogenarated_files","priority_list.txt"))),"r") as np_file:
                 print("")
                 print(np_file.read())
         elif input0 == ("cls"):
@@ -76,24 +78,24 @@ def priority_list():
                 priority_list()
                 break
             elif confirmation == ("y") or confirmation == ("Y"):
-                file = open(r"C:\Users\athar\OneDrive\Documents\projects\hidden_works\priority_list.txt","w")
+                file = open(((os.path.join(main_path,"autogenarated_files","priority_list.txt"))),"w")
                 file.close()
                 print("CLEARED THE CONTENT OF "+ '"'+"priority_list.txt"+'"')
             else:
                 priority_list()
                 break
         else:
-            with open(r"C:\Users\athar\OneDrive\Documents\projects\hidden_works\priority_list.txt", "a") as np_file:
+            with open(((os.path.join(main_path,"autogenarated_files","priority_list.txt"))),"a") as np_file:
                 np_file.write(input0+ "\n")
 
 def quick_note():
-    check_file = os.path.isfile(r"C:\Users\athar\OneDrive\Documents\projects\hidden_works\quick_note.txt")
+    check_file = os.path.isfile(os.path.join(main_path,"autogenarated_files","quick_note.txt"))
     if check_file == (False):
-        os.system("echo> "+r"C:\Users\athar\OneDrive\Documents\projects\hidden_works\quick_note.txt")
-    with open(r"C:\Users\athar\OneDrive\Documents\projects\hidden_works\quick_note.txt", "r") as np_file:    
+        open((os.path.join(main_path,"autogenarated_files","quick_note.txt")),"w").close()
+    with open((os.path.join(main_path,"autogenarated_files","quick_note.txt")),"r") as np_file:    
             data = np_file.read()
             number_of_characters = len(data)
-    if number_of_characters == 12 or number_of_characters == 0:
+    if number_of_characters == 0:
         print()
         print("EMPTY FILE AF")
         print()
@@ -105,15 +107,15 @@ def quick_note():
         if input0 == (""):
             break
         elif input0 == ("open"):
-            os.startfile(r"C:\Users\athar\OneDrive\Documents\projects\hidden_works\quick_note.txt")
+            os.startfile((os.path.join(main_path,"autogenarated_files","quick_note.txt")))
             break
         elif "*" in input0:
             replace_task = input0.replace("*"," ")
-            with open(r"C:\Users\athar\OneDrive\Documents\projects\hidden_works\quick_note.txt", "a") as np_file:
+            with open((os.path.join(main_path,"autogenarated_files","quick_note.txt")),"a") as np_file:
                 np_file.write(replace_task+ "\n")
                 break
         elif input0 == ("read"):
-            with open(r"C:\Users\athar\OneDrive\Documents\projects\hidden_works\quick_note.txt", "r") as np_file:
+            with open((os.path.join(main_path,"autogenarated_files","quick_note.txt")),"r") as np_file:
                 print("")
                 print(np_file.read())
         elif input0 == ("cls"):
@@ -124,24 +126,24 @@ def quick_note():
                 quick_note()
                 break
             elif confirmation == ("y") or confirmation == ("Y"):
-                file = open(r"C:\Users\athar\OneDrive\Documents\projects\hidden_works\quick_note.txt","w")
+                file = open((os.path.join(main_path,"autogenarated_files","quick_note.txt")),"w")
                 file.close()
                 print("CLEARED THE CONTENT OF "+ '"'+"quick_note.txt"+'"')
             else:
                 quick_note()
                 break
         else:
-            with open(r"C:\Users\athar\OneDrive\Documents\projects\hidden_works\quick_note.txt", "a") as np_file:
+            with open((os.path.join(main_path,"autogenarated_files","quick_note.txt")),"a") as np_file:
                 np_file.write(input0+ "\n")
 
 def roadmap():
-    check_file = os.path.isfile(r"C:\Users\athar\OneDrive\Documents\projects\hidden_works\roadmap.txt")
+    check_file = os.path.isfile(os.path.join(main_path,"autogenarated_files","roadmap.txt"))
     if check_file == (False):
-        os.system("echo> "+r"C:\Users\athar\OneDrive\Documents\projects\hidden_works\roadmap.txt")
-    with open(r"C:\Users\athar\OneDrive\Documents\projects\hidden_works\roadmap.txt", "r") as np_file:    
+        open((os.path.join(main_path,"autogenarated_files","roadmap.txt")),"w").close()
+    with open((os.path.join(main_path,"autogenarated_files","roadmap.txt")),"r") as np_file:    
             data = np_file.read()
             number_of_characters = len(data)
-    if number_of_characters == 12 or number_of_characters == 0:
+    if number_of_characters == 0:
         print()
         print("EMPTY FILE AF")
         print()
@@ -153,15 +155,15 @@ def roadmap():
         if input0 == (""):
             break
         elif input0 == ("open"):
-            os.startfile(r"C:\Users\athar\OneDrive\Documents\projects\hidden_works\roadmap.txt")
+            os.startfile((os.path.join(main_path,"autogenarated_files","roadmap.txt")))
             break
         elif "*" in input0:
             replace_task = input0.replace("*"," ")
-            with open(r"C:\Users\athar\OneDrive\Documents\projects\hidden_works\roadmap.txt", "a") as np_file:
+            with open((os.path.join(main_path,"autogenarated_files","roadmap.txt")),"a") as np_file:
                 np_file.write(replace_task+ "\n")
                 break
         elif input0 == ("read"):
-            with open(r"C:\Users\athar\OneDrive\Documents\projects\hidden_works\roadmap.txt", "r") as np_file:
+            with open((os.path.join(main_path,"autogenarated_files","roadmap.txt")),"r") as np_file:
                 print("")
                 print(np_file.read())
         elif input0 == ("cls"):
@@ -172,14 +174,14 @@ def roadmap():
                 roadmap()          
                 break
             elif confirmation == ("y") or confirmation == ("Y"):
-                file = open(r"C:\Users\athar\OneDrive\Documents\projects\hidden_works\roadmap.txt","w")
+                file = open((os.path.join(main_path,"autogenarated_files","roadmap.txt")),"w")
                 file.close()
                 print("CLEARED THE CONTENT OF "+ '"'+"roadmap.txt"+'"')
             else:
                 roadmap()
                 break
         else:
-            with open(r"C:\Users\athar\OneDrive\Documents\projects\hidden_works\roadmap.txt", "a") as np_file:
+            with open((os.path.join(main_path,"autogenarated_files","roadmap.txt")),"a") as np_file:
                 np_file.write(input0+ "\n")
 
             
