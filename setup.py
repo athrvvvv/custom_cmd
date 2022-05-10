98d7ace0258ad946bbc9a10834fb3a6e5a5f0f10
athrvvv <codingbruh.ac@gmail.com>
2022-05-10 22:34:26 +0530

    Minor changes

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from cx_Freeze import setup, Executable
   
-setup(name = "BABU" ,
+setup(name = "Babu" ,
       version = "1.0.0" ,
       description = "A.I Functionality :)" ,
       executables = [Executable("babu.py")])
