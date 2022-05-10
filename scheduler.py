e5978dec6dc8f05c0c94d7519af97b25bf4145a6
athrvvv <codingbruh.ac@gmail.com>
2022-05-10 22:33:27 +0530

    Upgraded reading & writing of lists

--- scheduler.py
+++ scheduler.py
@@ -7,7 +7,7 @@ str_today = str(now)
 main_path = os.path.dirname(__file__)
 
 def priority_reader():
-    with open((os.path.join((os.path.dirname(main_path)),"autogenarated_files","priority_list.txt")),"r") as f:
+    with open((os.path.join(main_path,"autogenarated_files","priority_list.txt")), "r") as f:
         print("PRIORITY AS BELOW:- ")
         Lines = f.readlines()   
         count = 0
@@ -19,17 +19,34 @@ def priority_reader():
     print()
 
 def set_on_off_prio(self):
-    with open((os.path.join(main_path,"autogenarated_files","prio_status.txt")), "w") as f:
-        status = self.lower()
-        f.write(status)
+    if self == "prio-":
+        with open((os.path.join(main_path,"autogenarated_files","prio_status.txt")), "r") as f:
+            data = f.read()
+            if data == ("on"):
+                print("PRIORITY LIST DISPLAYING AT STARTUP: ON")
+            elif data == ("off"):
+                print("PRIORITY LIST DISPLAYING AT STARTUP: OFF")
+            else:
+                print("None")
+    else:
+        parcel = self.replace("prio-","").lower()
+        if parcel == ("on") or parcel == ("off"):
+            with open((os.path.join(main_path,"autogenarated_files","prio_status.txt")), "w") as f:
+                f.write(parcel)
+                print("Priority list Status Updated")
+        else:
+            ("You can only set status to ON or OFF")
         
 def check_prio_status():
     with open((os.path.join(main_path,"autogenarated_files","prio_status.txt")), "r") as f:
         read = f.read()
         if read == ("on"):
             priority_reader()
-        else:
+        elif read == ("off"):
             StopIteration()
+        else:
+            priority_reader()
+            
 def priority_list():
     check_file = os.path.isfile(os.path.join(main_path,"autogenarated_files","priority_list.txt"))
     if check_file == (False):
@@ -45,6 +62,7 @@ def priority_list():
             not_space = line.rstrip()
             if not_space:
                 print("{}. {}".format(count, line.strip()))
+    print()
     
     if number_of_characters == 0:
         print()
@@ -136,11 +154,52 @@ def quick_note():
             with open((os.path.join(main_path,"autogenarated_files","quick_note.txt")),"a") as np_file:
                 np_file.write(input0+ "\n")
 
-def roadmap():
-    check_file = os.path.isfile(os.path.join(main_path,"autogenarated_files","roadmap.txt"))
+def todo_reader():
+    with open((os.path.join(main_path,"autogenarated_files","todo.txt")), "r") as f:
+        print("TODO LIST:- ")
+        Lines = f.readlines()   
+        count = 0
+        for line in Lines:
+            count += 1
+            not_space = line.rstrip()
+            if not_space:
+                print("{}. {}".format(count, line.strip())) 
+    print()
+
+def set_on_off_todo(self):
+    if self == "todo-":
+        with open((os.path.join(main_path,"autogenarated_files","todo_status.txt")), "r") as f:
+            data = f.read()
+            if data == ("on"):
+                print("TODO LIST DISPLAYING AT STARTUP: ON")
+            elif data == ("off"):
+                print("TODO LIST DISPLAYING AT STARTUP: OFF")
+            else:
+                print("None")
+    else:
+        parcel = self.replace("todo-","").lower()
+        if parcel == ("on") or parcel == ("off"):
+            with open((os.path.join(main_path,"autogenarated_files","todo_status.txt")), "w") as f:
+                f.write(parcel)
+                print("Todo list Status Updated")
+        else:
+            ("You can only set status to ON or OFF")
+        
+def check_todo_status():
+    with open((os.path.join(main_path,"autogenarated_files","todo_status.txt")), "r") as f:
+        read = f.read()
+        if read == ("on"):
+            todo_reader()
+        elif read == ("off"):
+            StopIteration()
+        else:
+            todo_reader()
+            
+def todo():
+    check_file = os.path.isfile(os.path.join(main_path,"autogenarated_files","todo.txt"))
     if check_file == (False):
-        open((os.path.join(main_path,"autogenarated_files","roadmap.txt")),"w").close()
-    with open((os.path.join(main_path,"autogenarated_files","roadmap.txt")),"r") as np_file:    
+        open((os.path.join(main_path,"autogenarated_files","todo.txt")),"w").close()
+    with open((os.path.join(main_path,"autogenarated_files","todo.txt")),"r") as np_file:    
             data = np_file.read()
             number_of_characters = len(data)
     if number_of_characters == 0:
@@ -155,15 +214,15 @@ def roadmap():
         if input0 == (""):
             break
         elif input0 == ("open"):
-            os.startfile((os.path.join(main_path,"autogenarated_files","roadmap.txt")))
+            os.startfile((os.path.join(main_path,"autogenarated_files","todo.txt")))
             break
         elif "*" in input0:
             replace_task = input0.replace("*"," ")
-            with open((os.path.join(main_path,"autogenarated_files","roadmap.txt")),"a") as np_file:
+            with open((os.path.join(main_path,"autogenarated_files","todo.txt")),"a") as np_file:
                 np_file.write(replace_task+ "\n")
                 break
         elif input0 == ("read"):
-            with open((os.path.join(main_path,"autogenarated_files","roadmap.txt")),"r") as np_file:
+            with open((os.path.join(main_path,"autogenarated_files","todo.txt")),"r") as np_file:
                 print("")
                 print(np_file.read())
         elif input0 == ("cls"):
@@ -171,17 +230,17 @@ def roadmap():
             print("YES (Y) or NO (N)")
             confirmation = input_source.getkey()
             if confirmation == ("n") or confirmation == ("N"):
-                roadmap()          
+                todo()          
                 break
             elif confirmation == ("y") or confirmation == ("Y"):
-                file = open((os.path.join(main_path,"autogenarated_files","roadmap.txt")),"w")
+                file = open((os.path.join(main_path,"autogenarated_files","todo.txt")),"w")
                 file.close()
-                print("CLEARED THE CONTENT OF "+ '"'+"roadmap.txt"+'"')
+                print("CLEARED THE CONTENT OF "+ '"'+"todo.txt"+'"')
             else:
-                roadmap()
+                todo()
                 break
         else:
-            with open((os.path.join(main_path,"autogenarated_files","roadmap.txt")),"a") as np_file:
+            with open((os.path.join(main_path,"autogenarated_files","todo.txt")),"a") as np_file:
                 np_file.write(input0+ "\n")
 
             
